b55d2e69bd2c998d0edda5df6682319d48e0c7ba
yan-cunha <yancunha.dev@gmail.com>
2024-08-11 12:11:22 -0400

    adição da logo nas telas

--- app/src/main/java/com/yan/foia/ui/fragments/signup/SignupFragment.kt
+++ app/src/main/java/com/yan/foia/ui/fragments/signup/SignupFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -14,6 +15,7 @@ class SignupFragment : Fragment() {
     private lateinit var backButton: ImageButton
     private lateinit var eyeButton: ImageButton
     private lateinit var eyeButtonConfirm: ImageButton
+    private lateinit var signupButton: Button
     private var _binding: FragmentSignupBinding? = null
 
     private val binding get() = _binding!!

ac35a80e01171d63a90f7e9c0d8454abfb8a0cdb
Yan Cunha <yancunha.dev@gmail.com>
2024-08-13 22:57:47 -0400

    fix: conserto das telas do menu logado que não estavam sendo infladas

--- app/src/main/java/com/yan/foia/ui/logged/fragments/history/HistoryFragment.kt
+++ app/src/main/java/com/yan/foia/ui/logged/fragments/history/HistoryFragment.kt
@@ -1,6 +1,29 @@
 package com.yan.foia.ui.logged.fragments.history
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.yan.foia.databinding.FragmentHistoryBinding
+import com.yan.foia.databinding.FragmentIdentifiedBinding
 
 class HistoryFragment : Fragment() {
+    private var _binding: FragmentHistoryBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+//        val welcomeViewModel = ViewModelProvider(this)[WelcomeViewModel::class.java]
+
+        _binding = FragmentHistoryBinding.inflate(layoutInflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
 }

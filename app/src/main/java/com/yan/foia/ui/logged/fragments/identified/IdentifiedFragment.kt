1658094a84a6b62f71bc8501156b7a97eb64a365
João Pedro <teixeira.santo@ufms.br>
2024-08-16 14:24:32 -0400

    Tela de identificao pela metade

--- app/src/main/java/com/yan/foia/ui/logged/fragments/identified/IdentifiedFragment.kt
+++ app/src/main/java/com/yan/foia/ui/logged/fragments/identified/IdentifiedFragment.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewbinding.ViewBindings
+import com.skydoves.expandablelayout.ExpandableLayout
+import com.yan.foia.R
 import com.yan.foia.databinding.FragmentIdentifiedBinding
 import com.yan.foia.ui.unlogged.fragments.welcome.WelcomeViewModel
 
@@ -13,6 +16,7 @@ class IdentifiedFragment : Fragment() {
     private var _binding: FragmentIdentifiedBinding? = null
 
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +29,21 @@ class IdentifiedFragment : Fragment() {
         _binding = FragmentIdentifiedBinding.inflate(layoutInflater, container, false)
         val root: View = binding.root
 
+        val ex = view?.findViewById<ExpandableLayout>(R.id.expandable) //recupera o id
+        var g = 0
+        //duvida no comportamento e implicações do operador non-nullable: '?.'
+        ex?.parentLayout?.setOnClickListener{
+            if (g==0){
+                g = 1
+                ex.expand()
+            }
+            else{
+                g = 0
+                ex.collapse()
+            }
+        }
+
         return root
     }
+
 }

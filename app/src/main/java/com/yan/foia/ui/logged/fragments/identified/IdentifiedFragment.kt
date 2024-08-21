db8c3464ce4de1a91857007508ae95d19673da35
TeixeiraES <teixeira.santo@ufms.br>
2024-08-21 19:15:10 -0400

    Texto imagem e dropdown ajustados

--- app/src/main/java/com/yan/foia/ui/logged/fragments/identified/IdentifiedFragment.kt
+++ app/src/main/java/com/yan/foia/ui/logged/fragments/identified/IdentifiedFragment.kt
@@ -5,12 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.viewbinding.ViewBindings
 import com.skydoves.expandablelayout.ExpandableLayout
 import com.yan.foia.R
 import com.yan.foia.databinding.FragmentIdentifiedBinding
-import com.yan.foia.ui.unlogged.fragments.welcome.WelcomeViewModel
 
 class IdentifiedFragment : Fragment() {
     private var _binding: FragmentIdentifiedBinding? = null
@@ -29,7 +26,7 @@ class IdentifiedFragment : Fragment() {
         _binding = FragmentIdentifiedBinding.inflate(layoutInflater, container, false)
         val root: View = binding.root
 
-        val ex = view?.findViewById<ExpandableLayout>(R.id.expandable) //recupera o id
+        val ex = view?.findViewById<ExpandableLayout>(R.id.description_expand) //recupera o id
         var g = 0
         //duvida no comportamento e implicações do operador non-nullable: '?.'
         ex?.parentLayout?.setOnClickListener{
@@ -49,18 +46,43 @@ class IdentifiedFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val ex = view?.findViewById<ExpandableLayout>(R.id.expandable) //recupera o id
-        var g = 0
+        val desc = view.findViewById<ExpandableLayout>(R.id.description_expand) //recupera o id
+        val rec = view.findViewById<ExpandableLayout>(R.id.recommend_expand) //recupera o id
+        //expandable de "produto", ainda nao adicionado:
+        //val prod = view.findViewById<ExpandableLayout>(R.id.description_expand) //recupera o id
+
+        //variáveis de controle da abertura dos 3 expandable layouts
+        var g1 = 0
+        var g2 = 0
+        //var g3 = 0
         //duvida no comportamento e implicações do operador non-nullable: '?.'
-        ex?.parentLayout?.setOnClickListener{
-            if (g==0){
-                g = 1
-                ex.expand()
+        desc?.parentLayout?.setOnClickListener{
+            if (g1==0){
+                g1 = 1
+                desc.expand()
+                if (g2==1){
+                    g2 = 0
+                    rec.collapse()}
+                //if (g3==1){prod.collapse()}
             }
             else{
-                g = 0
-                ex.collapse()
+                g1 = 0
+                desc.collapse()
             }
+
     }
+        rec?.parentLayout?.setOnClickListener{
+            if (g2==0){
+                g2 = 1
+                rec.expand()
+                if (g1==1){desc.collapse()}
+                //if (g3==1){prod.collapse()}
+            }
+            else{
+                g2 = 0
+                rec.collapse()
+            }
+
+        }
 
 }}
